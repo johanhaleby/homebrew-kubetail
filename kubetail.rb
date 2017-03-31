469538d7d9c71466742ba26523cd0850998b1524
Johan Haleby <johan.haleby@gmail.com>
2017-03-31 19:15:17 +0200

    Updating to 1.2.1 release

--- kubetail.rb
+++ kubetail.rb
@@ -1,8 +1,8 @@
 class Kubetail < Formula
   desc "Tail logs from multiple Kubernetes pods at the same time"
   homepage "https://github.com/johanhaleby/kubetail"
-  url "https://github.com/johanhaleby/kubetail/archive/1.2.0.tar.gz"
-  sha256 "3716a2899d99e25f26fb1ae4fe6bba3334329ce725f0efa403654172e75a30eb"
+  url "https://github.com/johanhaleby/kubetail/archive/1.2.1.tar.gz"
+  sha256 "734d732d93c81c4e849054fc646e8c759da3bea78e2b5efef39d27494e773752"
   
   def install
     bin.install "kubetail"
