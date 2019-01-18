d9ef72502a36b341669de8dad7e0692e8c4f89e3
Johan Haleby <johan.haleby@gmail.com>
2019-01-18 11:41:58 +0100

    Fixed version number

--- kubetail.rb
+++ kubetail.rb
@@ -1,7 +1,7 @@
 class Kubetail < Formula
   desc "Tail logs from multiple Kubernetes pods at the same time"
   homepage "https://github.com/johanhaleby/kubetail"
-  url "https://github.com/johanhaleby/kubetail/archive/1.6.5.tar.gz"
+  url "https://github.com/johanhaleby/kubetail/archive/1.6.6.tar.gz"
   sha256 "f30ac23e5411add15e85b1b7984dfb4151bd2b4ca1e750c47106dce6d0d7a473"
   head "https://github.com/johanhaleby/kubetail.git"
 
