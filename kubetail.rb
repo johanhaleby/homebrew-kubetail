4a3ba9be618ea03ba5cebb9ba129763a546e3b78
Johan Haleby <johan.haleby@gmail.com>
2022-04-13 08:28:02 +0200

    Releasing 1.6.15

--- kubetail.rb
+++ kubetail.rb
@@ -1,8 +1,8 @@
 class Kubetail < Formula
   desc "Tail logs from multiple Kubernetes pods at the same time"
   homepage "https://github.com/johanhaleby/kubetail"
-  url "https://github.com/johanhaleby/kubetail/archive/1.6.14.tar.gz"
-  sha256 "9e67660c3cc69150eac444f8274a1f148ed6d5170b9ab87e68900333cf1c6843"
+  url "https://github.com/johanhaleby/kubetail/archive/1.6.15.tar.gz"
+  sha256 "e9e7f6e26db9e177c972875780f998761534b6f29d7a59ab3042958b78935f99"
   head "https://github.com/johanhaleby/kubetail.git"
 
   option "with-short-names", "link as \"kt\" instead"
